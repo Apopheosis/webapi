6454d9d65a37a84d8cb945b24704af6a180492f5
Apopheosis <kwatar12@gmail.com>
2022-05-15 17:03:46 +0300

    Initial commit

---
+++ Controllers/RegionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using webapi;
+
+[ApiController]
+[Route("[controller]")]
+public class RegionController : ControllerBase
+{
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public RegionController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetRegion")]
+    public IEnumerable<Region> Get()
+    {
+        Region[] regions = {(new Region(){Name="France", Capital = "Paris", Continent = "Europe", Population = 300000}), 
+        (new Region(){Name="Kenya", Capital="Nairobi", Continent = "Africa", Population = 5000}), 
+        (new Region(){Name="Japan", Capital = "Tokyo", Continent = "Asia", Population = 500000000}),
+        (new Region(){Name="USA", Capital = "Washington", Continent = "North America", Population = 600000000}),
+        (new Region(){Name="Brazil", Capital="Rio", Continent = "South America", Population = 40000000}),
+        (new Region(){Name="Australia", Capital = "Canberra", Continent = "Australia", Population = 7000000})};
+        return regions;
+    }
+}
---
+++ Models/Region.cs
@@ -0,0 +1,10 @@
+﻿namespace webapi
+{
+    public class Region
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public string Continent { get; set; }
+    }
+}
